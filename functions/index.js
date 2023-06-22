8f7d93af7b886eee828977516e758880cdb07cfd
Samuel Holmez <samuelholmez@Samuels-MacBook-Air.local>
2023-06-22 14:59:03 -0400

    About to test two endponts

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+import {onRequest} from "firebase-functions/v2/https";
+import logger from "firebase-functions/logger";
+import express from "express"
+import cors from "cors"
+import { getAllCandy, addNewCandy } from "./src/candy.js";
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+app.get("/test", (req, res) => res.send("its working"))
+
+app.get("/candy", getAllCandy);
+app.post("/candy", addNewCandy);
+
+
+
+export const api =  onRequest({ maxInstances: 10}, app);
